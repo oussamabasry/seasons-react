6a11c889de3e8ddbcfef696c751ef23fade76561
oussamabasry <obasry@gmail.com>
2020-12-09 17:32:10 +0100

    last commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
+import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+
+
+
+class App extends React.Component {
+
+  state = {
+    lat: null,
+    errorMessage: ''
+  };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+
+  rendrerContent() {
+
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+
+    return <Spinner message="Please accept location request" />
+  }
+
+  render() {
+
+    return (
+      <div>
+        {this.rendrerContent()}
+      </div>
+    );
+  };
+
+};
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
